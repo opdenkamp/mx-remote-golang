402ef0769982d961f2213367b828c497d7dac387
Lars Op den Kamp <lars@opdenkamp-it.nl>
2026-08-28 17:57:18 +0200

    chore: bump to 2.1.0

--- constants.go
+++ constants.go
@@ -6,7 +6,7 @@ package mxremote
 // Library and protocol versions.
 const (
 	// Version is the version of this library.
-	Version = "2.0.0"
+	Version = "2.1.0"
 
 	// ProtocolVersion is the highest MX Remote protocol version understood.
 	ProtocolVersion uint16 = 0x28
